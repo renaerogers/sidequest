473748a2cff5d960569ce645bcc69ac55a643878
Renae Rogers <renae.a.rogers@gmail.com>
2026-04-01 10:00:41 -0600

    moved gamemaps code over to gamemoves

--- pc-js/gamemaps.js
+++ pc-js/gamemaps.js
@@ -1,72 +1,5 @@
-// Get Scrubby 
-const scrubby = {
-    'sitting': document.getElementById('scrub_sit'),
-    'runL1': document.getElementById('scrub_run_L1'),
-    'runL2': document.getElementById('scrub_run_L2'),
-    'runR1': document.getElementById('scrub_run_R1'),
-    'runR2': document.getElementById('scrub_run_R2'),
-    'runU1': document.getElementById('scrub_run_U1'),
-    'runU2': document.getElementById('scrub_run_U2'),
-    'runD1': document.getElementById('scrub_run_D1'),
-    'runD2': document.getElementById('scrub_run_D2'),
-    'scrubby-block': document.getElementById('scrubby-block')
-};
-// Get map content
-const mapLibrary = document.getElementById('map-library');
-
-const mapMaze = document.getElementById('map-maze');
-
-const mapGallery = document.getElementById('map-gallery');
-
 // Get 'portals'
 // const moveToMaze = document.getElementById('library-top-left-carpet');
-
-let map = 'library'; // Initial map
-
-// Function to change the map displayed
-function showMap(mapName) {
-    if (mapName === 'library') {
-        mapLibrary.style.display = 'block';
-        mapMaze.style.display = 'none';
-        mapGallery.style.display = 'none';
-    } else if (mapName === 'maze') {
-        mapLibrary.style.display = 'none';
-        mapMaze.style.display = 'block';
-        mapGallery.style.display = 'none';
-    } else if (mapName === 'gallery') {
-        mapLibrary.style.display = 'none';
-        mapMaze.style.display = 'none';
-        mapGallery.style.display = 'block';
-    }
-}
-
-// Function to change Scrubby's position
-function updatePositionBounceBack(runLeft, runRight, runUp, runDown) {
-    const bounceBackSpeed = speed * 2; 
-    if (runLeft) {
-        x += bounceBackSpeed;
-    }
-    if (runRight) {
-        x -= bounceBackSpeed;
-    }
-    if (runUp) {
-        y += bounceBackSpeed;
-    }
-    if (runDown) {
-        y -= bounceBackSpeed;
-    }
-
-    const transformString = `translate(${x}px, ${y}px)`;
-    const allFrames = [
-        scrubbySitting, scrubbyRunL1, scrubbyRunL2, 
-        scrubbyRunR1, scrubbyRunR2, scrubbyRunU1, 
-        scrubbyRunU2, scrubbyRunD1, scrubbyRunD2, scrubbyBlock
-    ];
-
-    allFrames.forEach(scrubby => {
-        scrubby.style.transform = transformString;
-    });
-}
 
 // Function to change Scrubby's size depending on the map displayed 
 // function updateScrubbySize(mapName) {
@@ -85,65 +18,27 @@ function updatePositionBounceBack(runLeft, runRight, runUp, runDown) {
 //     }
 // }
 
-function updateScrubbySize(mapName) {
-    // Determine the width based on the mapName
-    const sizes = {
-        'library': '180px',
-        'gallery': '180px',
-        'maze':    '120px',
-        'forest':  '150px' // Easy to add more later!
-    };
-    const newWidth = sizes[mapName]
+// function updateScrubbyBlock(mapName) {
+//     console.log(mapName + "Please finish this function.")
+
+//     const sizes = {
+//         'library': '180px',
+//         'gallery': '180px',
+//         'maze':    '120px',
+//     };
+//     const newWidth = sizes[mapName]
     
-    if (newWidth) {
-        Object.entries(scrubby)
-            .filter(([key]) => key !== 'scrubby-block')
-            .forEach(([key, element]) => {
-                if (element) {
-                    element.style.width = newWidth;
-                }
-            });
-    }   
-}
+//     if (newWidth) {
+//         Object.entries(scrubby)
+//             .filter(([key]) => key !== 'scrubby-block')
+//             .forEach(([key, element]) => {
+//                 if (element) {
+//                     element.style.width = newWidth;
+//                 }
+//             });
+//             scrubby.scrubby-block.styles.width - '60'
+//             scrubby["scrubby-block"].styles.width - '60'
 
-function updateScrubbyBlock(mapName) {
-    console.log(mapName + "Please finish this function.")
-}
+//     }   
+// }
 
-const moveToMaze = document.getElementById('point1');
-
-function checkCollision() {
-    const rectOne = scrubby["scrubby-block"].getBoundingClientRect();
-    const rectTwo = moveToMaze.getBoundingClientRect();
-
-    if (rectOne.left < rectTwo.right &&
-        rectOne.right > rectTwo.left &&
-        rectOne.top < rectTwo.bottom &&
-        rectOne.bottom > rectTwo.top && map == 'library') {
-        scrubby["scrubby-block"].style.backgroundColor = 'black';
-        showMap('maze'); 
-        updateScrubbySize('maze')
-
-        let map = 'maze'
-        
-        // const runLeft = keys['a'] || keys['arrowleft'];
-        // const runRight = keys['d'] || keys['arrowright'];
-        // const runUp = keys['w'] || keys['arrowup'];
-        // const runDown = keys['s'] || keys['arrowdown'];
-
-        // updatePositionBounceBack(runLeft, runRight, runUp, runDown);
-    } else if (rectOne.left < rectTwo.right &&
-        rectOne.right > rectTwo.left &&
-        rectOne.top < rectTwo.bottom &&
-        rectOne.bottom > rectTwo.top && map == 'library'){
-        moveToMaze.style.backgroundColor = 'purple';
-        showMap('library')
-        updateScrubbySize('library')
-
-        scrubby["scrubby-block"].style.backgroundColor = 'yellow';
-    } else {
-        moveToMaze.style.backgroundColor = 'purple';
-    }
-}
-
-setInterval(checkCollision, 500);
--- pc-js/gamemoves.js
+++ pc-js/gamemoves.js
@@ -1,3 +1,17 @@
+// Get Scrubby elements
+const scrubby = {
+    'sitting': document.getElementById('scrub_sit'),
+    'runL1': document.getElementById('scrub_run_L1'),
+    'runL2': document.getElementById('scrub_run_L2'),
+    'runR1': document.getElementById('scrub_run_R1'),
+    'runR2': document.getElementById('scrub_run_R2'),
+    'runU1': document.getElementById('scrub_run_U1'),
+    'runU2': document.getElementById('scrub_run_U2'),
+    'runD1': document.getElementById('scrub_run_D1'),
+    'runD2': document.getElementById('scrub_run_D2'),
+    'scrubby-block': document.getElementById('scrubby-block')
+};
+
 const scrubbySitting = document.getElementById('scrub_sit');
 const scrubbyRunL1 = document.getElementById('scrub_run_L1');
 const scrubbyRunL2 = document.getElementById('scrub_run_L2');
@@ -9,6 +23,30 @@ const scrubbyRunD1 = document.getElementById('scrub_run_D1');
 const scrubbyRunD2 = document.getElementById('scrub_run_D2');
 
 const scrubbyBlock = document.getElementById('scrubby-block');
+
+// Get map content
+const mapLibrary = document.getElementById('map-library');
+const mapMaze = document.getElementById('map-maze');
+const mapGallery = document.getElementById('map-gallery');
+
+let map = 'library'; // Initial map
+
+// Function to change the map displayed
+function showMap(mapName) {
+    if (mapName === 'library') {
+        mapLibrary.style.display = 'block';
+        mapMaze.style.display = 'none';
+        mapGallery.style.display = 'none';
+    } else if (mapName === 'maze') {
+        mapLibrary.style.display = 'none';
+        mapMaze.style.display = 'block';
+        mapGallery.style.display = 'none';
+    } else if (mapName === 'gallery') {
+        mapLibrary.style.display = 'none';
+        mapMaze.style.display = 'none';
+        mapGallery.style.display = 'block';
+    }
+}
 
 const keys = {};
 let frame = 0;
@@ -50,6 +88,73 @@ function updatePosition(runLeft, runRight, runUp, runDown) {
     allFrames.forEach(scrubby => {
         scrubby.style.transform = transformString;
     });
+}
+
+// Function to change Scrubby's position (post hitting a portal)
+function updatePositionBounceBack(runLeft, runRight, runUp, runDown) {
+    const bounceBackSpeed = speed * 2; 
+    if (runLeft) {
+        x += bounceBackSpeed;
+    }
+    if (runRight) {
+        x -= bounceBackSpeed;
+    }
+    if (runUp) {
+        y += bounceBackSpeed;
+    }
+    if (runDown) {
+        y -= bounceBackSpeed;
+    }
+
+    const transformString = `translate(${x}px, ${y}px)`;
+    const allFrames = [
+        scrubbySitting, scrubbyRunL1, scrubbyRunL2, 
+        scrubbyRunR1, scrubbyRunR2, scrubbyRunU1, 
+        scrubbyRunU2, scrubbyRunD1, scrubbyRunD2, scrubbyBlock
+    ];
+
+    allFrames.forEach(scrubby => {
+        scrubby.style.transform = transformString;
+    });
+}
+
+function updateScrubbyBlock(mapName, direction) {
+    // Use showMap function to check current map and adjust block size accordingly
+    if (mapName == 'maze') {
+        if (direction == 'runLeft') {
+            scrubbyBlock.style.width = '45px';
+            scrubbyBlock.style.height = '25px';
+        } else if (direction == 'right') {
+            scrubbyBlock.style.width = '45px';
+            scrubbyBlock.style.height = '25px';
+        } else if (direction == 'runUp') {
+            scrubbyBlock.style.width = '30px';
+            scrubbyBlock.style.height = '45px';
+        } else if (direction == 'runDown') {
+            scrubbyBlock.style.width = '30px';
+            scrubbyBlock.style.height = '45px';
+        } else {
+            scrubbyBlock.style.width = '35px';
+            scrubbyBlock.style.height = '40px';
+        }
+    } else {
+        if (direction == 'runLeft') {
+            scrubbyBlock.style.width = '90px';
+            scrubbyBlock.style.height = '50px';
+        } else if (direction == 'runRight') {
+            scrubbyBlock.style.width = '90px';
+            scrubbyBlock.style.height = '50px';
+        } else if (direction == 'runUp') {
+            scrubbyBlock.style.width = '60px';
+            scrubbyBlock.style.height = '90px';
+        } else if (direction == 'runDown') {
+            scrubbyBlock.style.width = '60px';
+            scrubbyBlock.style.height = '90px';
+        } else {
+            scrubbyBlock.style.width = '70px';
+            scrubbyBlock.style.height = '80px';
+        }
+    }
 }
 
 function startRunningLeftAnimation(runLeft) {
@@ -164,6 +269,29 @@ function startRunningDownAnimation(runDown) {
     }
 }
 
+function updateScrubbySize(mapName) {
+    // Determine the width based on the mapName
+    const sizes = {
+        'library': '180px',
+        'gallery': '180px',
+        'maze':    '120px',
+    };
+    const newWidth = sizes[mapName]
+    
+    if (newWidth) {
+        Object.entries(scrubby)
+            .filter(([key]) => key !== 'scrubby-block')
+            .forEach(([key, element]) => {
+                if (element) {
+                    element.style.width = newWidth;
+                }
+            });
+            scrubby.scrubby-block.styles.width - '60'
+            scrubby["scrubby-block"].styles.width - '60'
+
+    }   
+}
+
 function gameLoop() {
     const runLeft = keys['a'] || keys['arrowleft'];
     const runRight = keys['d'] || keys['arrowright'];
@@ -174,29 +302,60 @@ function gameLoop() {
 
     if (runLeft) {
         startRunningLeftAnimation(true);
-        scrubbyBlock.style.width = '90px';
-        scrubbyBlock.style.height = '50px';
+        updateScrubbyBlock(map, runLeft);
     } else if (runRight) {
         startRunningRightAnimation(true);
-        scrubbyBlock.style.width = '90px';
-        scrubbyBlock.style.height = '50px';
+        updateScrubbyBlock(map,runRight);
     } else if (runUp) {
         startRunningUpAnimation(true);
-        scrubbyBlock.style.width = '60px';
-        scrubbyBlock.style.height = '90px';
+        updateScrubbyBlock(map, runUp);
     } else if (runDown) {
         startRunningDownAnimation(true);
-        scrubbyBlock.style.width = '60px';
-        scrubbyBlock.style.height = '90px';
-
+        updateScrubbyBlock(map, runDown);
     } else {
         startRunningLeftAnimation(false);
         startRunningRightAnimation(false);
         startRunningUpAnimation(false);
         startRunningDownAnimation(false);
-        scrubbyBlock.style.width = '70px';
-        scrubbyBlock.style.height = '80px';
+        updateScrubbyBlock(map, false);
     }
 }
 setInterval(gameLoop, 200); 
 
+const moveToMaze = document.getElementById('point1');
+
+function checkCollision() {
+    const rectOne = scrubby["scrubby-block"].getBoundingClientRect();
+    const rectTwo = moveToMaze.getBoundingClientRect();
+
+    if (rectOne.left < rectTwo.right &&
+        rectOne.right > rectTwo.left &&
+        rectOne.top < rectTwo.bottom &&
+        rectOne.bottom > rectTwo.top && map == 'library') {
+        scrubby["scrubby-block"].style.backgroundColor = 'black';
+        showMap('maze'); 
+        updateScrubbySize('maze')
+
+        let map = 'maze'
+        
+        // const runLeft = keys['a'] || keys['arrowleft'];
+        // const runRight = keys['d'] || keys['arrowright'];
+        // const runUp = keys['w'] || keys['arrowup'];
+        // const runDown = keys['s'] || keys['arrowdown'];
+
+        // updatePositionBounceBack(runLeft, runRight, runUp, runDown);
+    } else if (rectOne.left < rectTwo.right &&
+        rectOne.right > rectTwo.left &&
+        rectOne.top < rectTwo.bottom &&
+        rectOne.bottom > rectTwo.top && map == 'library'){
+        moveToMaze.style.backgroundColor = 'purple';
+        showMap('library')
+        updateScrubbySize('library')
+
+        scrubby["scrubby-block"].style.backgroundColor = 'yellow';
+    } else {
+        moveToMaze.style.backgroundColor = 'purple';
+    }
+}
+
+setInterval(checkCollision, 500);
